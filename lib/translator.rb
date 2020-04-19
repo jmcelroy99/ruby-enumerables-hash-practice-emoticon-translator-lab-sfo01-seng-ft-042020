4a6eea5742c8e4cf453bd954f00425d7fe05a130
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-19 23:18:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,9 @@ def get_japanese_emoticon
   japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
-def get_english_meaning
+def get_english_meaning(emoticon_file, emoticon)
   # code goes here
+  emoticon_lib = load_library(emoticon_file)
+  english_meaning = emoticon_lib['get_meaning'][emoticon]
+  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
